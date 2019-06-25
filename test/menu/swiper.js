a2a8152212a2b3140caee5eb5eace0f69f19d55a
SantiagoOlivera <olivera.santiagoariel@gmail.com>
2019-06-24 22:50:11 -0300

    multiple slider istances

--- test/menu/swiper.js
+++ test/menu/swiper.js
@@ -1,7 +1,23 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView:  4,
-    slidesPerGroup: 4,
-    spaceBetween: 0, 
+var swiper1 = new Swiper('.swiper1', {
+    slidesPerView:  1,
+    slidesPerGroup: 1,
+
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+var swiper5 = new Swiper('.swiper5', {
+    slidesPerView:  5,
+    slidesPerGroup: 5,
+    spaceBetween: 5, 
     breakpoints: {
         // when window width is >= 1200px
         1200: {
@@ -30,4 +46,3 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
-
